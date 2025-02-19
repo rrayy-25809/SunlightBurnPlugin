a6fdb59d80b5c6c8b5811d96c3a63ad9e9bc610c
rrayy <72653431+rrayy-25809@users.noreply.github.com>
2025-02-19 16:19:48 +0900

    Changes to be committed: 추가 데미지 변경 	modified:   src/main/java/com/rrayy/sunlightburn/SunlightBurnPlugin.java

--- src/main/java/com/rrayy/sunlightburn/SunlightBurnPlugin.java
+++ src/main/java/com/rrayy/sunlightburn/SunlightBurnPlugin.java
@@ -28,7 +28,7 @@ public class SunlightBurnPlugin extends JavaPlugin{
                         if (player.getWorld().getTime() < 12300 || player.getWorld().getTime() > 23850) { // Daytime check
                             if (player.getLocation().getBlock().getLightFromSky() == 15) { // Direct sunlight check
                                 player.setFireTicks(20); // Set player on fire for 1 second
-                                player.damage(1.0); // 추가 데미지 부여
+                                player.damage(6.0); // 추가 데미지 부여
                             }
                         }
                     }
