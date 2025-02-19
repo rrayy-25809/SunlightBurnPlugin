8293c7f33abeae0e035d07d0ddacd22655e470cb
rrayy <72653431+rrayy-25809@users.noreply.github.com>
2025-02-19 15:00:50 +0900

    Changes to be committed: 추가 데미지 부여 기능 추가 	modified:   src/main/java/com/rrayy/sunlightburn/SunlightBurnPlugin.java

--- src/main/java/com/rrayy/sunlightburn/SunlightBurnPlugin.java
+++ src/main/java/com/rrayy/sunlightburn/SunlightBurnPlugin.java
@@ -6,7 +6,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
-public class SunlightBurnPlugin extends JavaPlugin {
+public class SunlightBurnPlugin extends JavaPlugin{
 
     @Override
     public void onEnable() {
@@ -28,6 +28,7 @@ public class SunlightBurnPlugin extends JavaPlugin {
                         if (player.getWorld().getTime() < 12300 || player.getWorld().getTime() > 23850) { // Daytime check
                             if (player.getLocation().getBlock().getLightFromSky() == 15) { // Direct sunlight check
                                 player.setFireTicks(20); // Set player on fire for 1 second
+                                player.damage(1.0); // 추가 데미지 부여
                             }
                         }
                     }
